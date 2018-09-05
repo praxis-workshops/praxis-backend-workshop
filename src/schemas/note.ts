70d9815a5fb74a40707b9f46895984d3f116349d
Sebastian Pino Sánchez <spinos@unal.edu.co>
2018-09-04 23:53:04 -0500

    Deleting empty lines

--- src/schemas/note.ts
+++ src/schemas/note.ts
@@ -1,18 +1,15 @@
 import { Schema } from 'mongoose';
-import { UserSchema } from './user';
 
 export const NoteSchema = new Schema({
     text: {
         type: String,
         required: true
     },
-
     owner: {
         type: String,
         required: true
     }
 }, {
-
         timestamps: {
             createdAt: true,
             updatedAt: true
