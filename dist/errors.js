ddbc437a7fb499a8a42c12d0641cccb64ea20f4f
León Darío Peña L <leonplondon@gmail.com>
2018-09-05 01:14:11 -0500

    Fixed repo

--- dist/errors.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    request_without_payload: 'request_without_payload',
-    id_invalid_format: 'id_invalid_format',
-};
-//# sourceMappingURL=errors.js.map
